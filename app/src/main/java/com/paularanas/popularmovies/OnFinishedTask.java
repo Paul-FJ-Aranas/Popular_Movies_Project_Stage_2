05fc0849692121f0c790b5e82d2993e202522a64
Paul_FJ_Aranas <paul.aranas@ymail.com>
2015-08-19 10:58:20 -0700

    add rotation without api call, save scroll position move GridView to fragment, refactor

---
+++ app/src/main/java/com/paularanas/popularmovies/OnFinishedTask.java
@@ -0,0 +1,11 @@
+package com.paularanas.popularmovies;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Paul Aranas on 8/19/2015.
+ */
+public interface OnFinishedTask {
+        public void passData(ArrayList<Movie> movies);
+    }
+
